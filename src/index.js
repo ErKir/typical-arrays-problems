b75f3e64ec3023f477f6d6e775538a910abc0aad
ErKir <kirillerohin@gmail.com>
2020-09-29 23:33:22 +0300

    feat: add functions - min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+export function min (array) {
+    if (array.length === 0 || array === undefined) {
+        return 0;
+    }
+    const sorted = array.sort((a, b) => a - b);
+    return sorted[0];
+};
+
+export function max (array) {
+    if (array.length === 0 || array === undefined) {
+        return 0;
+    }
+    const sorted = array.sort((a, b) => b - a);
+    return sorted[0];
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+export function avg (array) {
+    if (array.length === 0 || array === undefined) {
+        return 0;
+    }
+    const sum = array.reduce((item, acc) => acc + item, 0);
+    const average = sum / (array.length + 1);
+};
